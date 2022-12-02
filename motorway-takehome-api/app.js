42561fbcb958242fa42c1d94918bf6ee9d068c57
Pawel <pawel.mysinski@gmail.com>
2022-12-02 01:07:42 +0000

    improved waitng for db connection

--- motorway-takehome-api/app.js
+++ motorway-takehome-api/app.js
@@ -16,14 +16,22 @@ app.use(globalErrorHandler);
 
 const initPromise = promiseRetry(
   async (retry) => {
-    await db.sequelize.authenticate().catch(retry);
+    console.log('Connecting to database...')
+    return db.sequelize.authenticate().catch((e) => {
+      console.error('Unable to connect to the database: ', e);
+      retry(e)
+    });
   },
   {
+    forever: true,
     factor: 1.2,
     minTimeout: 1000,
     maxTimeout: 2000
   }
-).then(() => db.sequelize.sync({ alter: true }));
+).then(() => {
+  console.log('Connected. Syncing...')
+  return db.sequelize.sync({ alter: true })
+});
 
 
 module.exports = initPromise.then(() => app);
